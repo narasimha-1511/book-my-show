ea416676c778bf74376e4ea155414c963e937a42
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-19 23:30:27 +0530

    completing the routes

--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -16,7 +16,29 @@ router.post(
   "/webhook",
   express.raw({ type: "application/json" }),
   (request, response) => {
-    //implement the webhook functionality
+    console.log("Webhook Called");
+    const sig = request.headers["stripe-signature"];
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    response.send();
   }
 );
 
@@ -27,16 +49,133 @@ async function handlePaymentIntentSucceeded(paymentIntent) {
 }
 
 router.post("/make-payment", async (req, res) => {
-  // implement the make payment functionality
+  try {
+    const { token, amount } = req.body;
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "usd",
+      customer: customer.id,
+      payment_method_types: ["card"],
+      receipt_email: token.email,
+      description: "Token has been assigned to the movie!",
+    });
+
+    // const charge = await stripe.charges.create({
+    //     amount: amount,
+    //     currency: "usd",
+    //     customer: customer.id,
+    //     receipt_email: token.email,
+    //     description: "Token has been assigned to the movie!"
+    // });
+
+    const transactionId = paymentIntent.id;
+
+    res.send({
+      success: true,
+      message: "Payment Successful! Ticket(s) booked!",
+      data: transactionId,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 // Create a booking after the payment
 router.post("/book-show", async (req, res) => {
-  // implement the book show functionality
+  try {
+    const newBooking = new Booking(req.body);
+    await newBooking.save();
+
+    const show = await Show.findById(req.body.show).populate("movie");
+    const updatedBookedSeats = [...show.bookedSeats, ...req.body.seats];
+    await Show.findByIdAndUpdate(req.body.show, {
+      bookedSeats: updatedBookedSeats,
+    });
+
+    const populatedBooking = await Booking.findById(newBooking._id)
+      .populate("user")
+      .populate("show")
+      .populate({
+        path: "show",
+        populate: {
+          path: "movie",
+          model: "movies",
+        },
+      })
+      .populate({
+        path: "show",
+        populate: {
+          path: "theatre",
+          model: "theatres",
+        },
+      });
+
+    console.log("this is populated Booking", populatedBooking);
+    // console.log(populatedBooking.user.email);
+
+    res.send({
+      success: true,
+      message: "New Booking done!",
+      data: populatedBooking,
+    });
+
+    await EmailHelper("ticketTemplate.html", populatedBooking.user.email, {
+      name: populatedBooking.user.name,
+      movie: populatedBooking.show.movie.title,
+      theatre: populatedBooking.show.theatre.name,
+      date: populatedBooking.show.date,
+      time: populatedBooking.show.time,
+      seats: populatedBooking.seats,
+      amount: populatedBooking.seats.length * populatedBooking.show.ticketPrice,
+      transactionId: populatedBooking.transactionId,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 router.get("/get-all-bookings", authMiddleware, async (req, res) => {
-  // implement the get all bookings functionality
+  try {
+    const bookings = await Booking.find({ user: req.body.userId })
+      .populate("user")
+      .populate("show")
+      .populate({
+        path: "show",
+        populate: {
+          path: "movie",
+          model: "movies",
+        },
+      })
+      .populate({
+        path: "show",
+        populate: {
+          path: "theatre",
+          model: "theatres",
+        },
+      });
+
+    res.send({
+      success: true,
+      message: "Bookings fetched!",
+      data: bookings,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 module.exports = router;
--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -4,29 +4,93 @@ const Movie = require("../models/movieModel");
 // Add a Movie
 
 router.post("/add-movie", async (req, res) => {
-  // implement the add movie functionality
+  try {
+    const newMovie = new Movie(req.body);
+    await newMovie.save();
+    res.send({
+      success: true,
+      message: "New movie has been added!",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
 // Get all the movies
 
 router.get("/get-all-movies", async (req, res) => {
-  // implement the get all movies functionality
+  try {
+    const allMovies = await Movie.find();
+    res.send({
+      success: true,
+      message: "All movies have been fetched!",
+      data: allMovies,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
 // Update a movie
 
 // Update a movie
 router.put("/update-movie", async (req, res) => {
-  // implementing the update movie over here
+  try {
+    const movie = await Movie.findByIdAndUpdate(req.body.movieId, req.body);
+    res.send({
+      success: true,
+      message: "The movie has been updated!",
+      data: movie,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 router.put("/delete-movie", async (req, res) => {
-  // deletingh a movie over here
+  try {
+    await Movie.findByIdAndDelete(req.body.movieId);
+    console.log(req.body.movieId);
+    res.send({
+      success: true,
+      message: "The movie has been deleted!",
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 // Fetch single movie by id
 router.get("/movie/:id", async (req, res) => {
-  // implement the get single movie by id functionality
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.send({
+      success: true,
+      message: "Movie fetched successfully!",
+      data: movie,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
+
+//APi end
+
+// update the movie , delete the movie , get all the movies
 
 module.exports = router;
