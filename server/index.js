470553f87d0c5bc42790465cd21b178cdba0193e
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-19 09:46:55 +0530

    server setup intialzed

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config();
+const router = require('./routes/test');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api', router);
+app.get('/' , (req , res) => {
+    res.json({
+        message: 'Working good'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on port 3000');
+});
