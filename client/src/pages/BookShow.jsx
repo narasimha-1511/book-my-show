56734c3a7c93b0f1b41d31f128e58f58dcd8c5e9
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-07-21 14:56:01 +0530

    finished the dash board for the user

--- client/src/pages/BookShow.jsx
+++ client/src/pages/BookShow.jsx
@@ -16,19 +16,156 @@ const BookShow = () => {
   const params = useParams();
   const navigate = useNavigate();
   const getData = async () => {
-    //get data and populate later
+    try {
+      dispatch(showLoading());
+      const response = await getShowById({ showId: params.id });
+      if (response.success) {
+        setShow(response.data);
+        // message.success(response.message);
+        console.log(response.data);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(hideLoading());
+    } catch (err) {
+      message.error(err.message);
+      dispatch(hideLoading());
+    }
   };
 
   const getSeats = () => {
-    // get seats here and return them
+    let columns = 12;
+    let totalSeats = show.totalSeats;
+    let rows = Math.ceil(totalSeats / columns);
+
+    return (
+      <div className="d-flex flex-column align-items-center">
+        <div className="w-100 max-width-600 mx-auto mb-25px">
+          <p className="text-center mb-10px">
+            Screen this side, you will be watching in this direction
+          </p>
+          <div className="screen-div"></div>
+        </div>
+        <ul className="seat-ul justify-content-center">
+          {Array.from(Array(rows).keys()).map((row) => {
+            return Array.from(Array(columns).keys()).map((column) => {
+              let seatNumber = row * columns + column + 1;
+
+              // Calculation for the first iteration
+              // 0*12 + 0+1 = 1
+              // 0*12 + 1+1 = 2
+              // 0*12 + 2+1 = 3
+              // So on up till 12th seat
+
+              // Calculation for the second iteration
+              // 1*12 + 0+1 = 13
+              // 1*12 + 1+1 = 14
+              // 1*12 + 2+1 = 15
+              // So on up till 24th seat
+
+              // Calculation for the third iteration
+              // 2*12 + 0+1 = 25
+              // 2*12 + 1+1 = 26
+              // 2*12 + 2+1 = 27
+              // So on up till 36th seat
+
+              // So on...
+
+              // this part
+
+              let seatClass = "seat-btn";
+
+              if (selectedSeats.includes(seatNumber)) {
+                seatClass += " selected";
+              }
+              if (show.bookedSeats.includes(seatNumber)) {
+                seatClass += " booked";
+              }
+
+              if (seatNumber <= totalSeats)
+                return (
+                  <li>
+                    <button
+                      onClick={() => {
+                        if (!show.bookedSeats.includes(seatNumber)) {
+                          if (selectedSeats.includes(seatNumber)) {
+                            setSelectedSeats(
+                              selectedSeats.filter(
+                                (curSeatNumber) => curSeatNumber !== seatNumber
+                              )
+                            );
+                          } else {
+                            setSelectedSeats([...selectedSeats, seatNumber]);
+                          }
+                        } else {
+                          // Optionally, show a message that the seat is booked and cannot be selected.
+                          console.log("This seat is already booked.");
+                        }
+                      }}
+                      className={seatClass}
+                    >
+                      {seatNumber}
+                    </button>
+                  </li>
+                );
+            });
+          })}
+        </ul>
+
+        <div className="d-flex bottom-card justify-content-between w-100 max-width-600 mx-auto mb-25px mt-3">
+          <div className="flex-1">
+            Selected Seats: <span>{selectedSeats.join(", ")}</span>
+          </div>
+          <div className="flex-shrink-0 ms-3">
+            Total Price:{" "}
+            <span>Rs. {selectedSeats.length * show.ticketPrice}</span>
+          </div>
+        </div>
+      </div>
+    );
   };
 
   const book = async (transactionId) => {
-    // book a seat with the following transaction id
+    try {
+      dispatch(showLoading());
+      const response = await bookShow({
+        show: params.id,
+        transactionId,
+        seats: selectedSeats,
+        user: user._id,
+      });
+      if (response.success) {
+        message.success("Show Booking done!");
+        navigate("/profile");
+      } else {
+        message.error(response.message);
+      }
+      dispatch(hideLoading());
+    } catch (err) {
+      message.error(err.message);
+      dispatch(hideLoading());
+    }
   };
 
   const onToken = async (token) => {
-    // this is crazy implementaisonf
+    try {
+      dispatch(showLoading());
+      const response = await makePayment(
+        token,
+        selectedSeats.length * show.ticketPrice * 100
+      );
+      if (response.success) {
+        message.success(response.message);
+        book(response.data);
+        console.log(response);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(hideLoading());
+    } catch (err) {
+      message.error(err.message);
+      dispatch(hideLoading());
+    }
   };
 
   useEffect(() => {
